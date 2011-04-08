59aedf3509f1945a521a40f4c56e33b47f715ddf
Adrian Rodriguez <iamadrianrodriguez@gmail.com>
2011-04-08 10:29:00 -0500

    Small start
      Changed the names of ie.scss and print.scss to ie.sass and print.sass
      Centered the container on the page and added some basic html5 elements, and the nav on the left side

--- config.rb
+++ config.rb
@@ -5,6 +5,5 @@ css_dir = "css"
 sass_dir = "src"
 images_dir = "images"
 javascripts_dir = "js"
-preffered_syntax :sass
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
