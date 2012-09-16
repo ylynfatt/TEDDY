c0ab91a8f07825e1ae5d2a55362c58009c017183
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2012-09-15 23:29:24 -0500

    test jQuery Masonry for  talk layout

--- js/master.js
+++ js/master.js
@@ -1,30 +1,43 @@
 $(function() {
+	
 	// Hide "About this Talk" initially, except the title
+	/*
 	$(".about_this_talk .meta").hide();
 	$(".about_this_talk .description").hide();
 	$(".about_this_talk .comment_share").hide();
 	$(".about_this_talk .ratings").hide();
-
+	 */
 	$(".talk").addClass("inactive");
 
 	// On hover, display information
 	$(".talk").hover(function () {
-		$(".talk").removeClass("inactive").addClass("active");
+		$(this).removeClass("inactive").addClass("active");
+		/*
+		$(this).hasClass("active").(".about_this_talk .meta").show();
+		$(this).(".about_this_talk .description").show();
+		$(this).(".about_this_talk .comment_share").show();
+		$(this).(".about_this_talk .ratings").show();	
 		
-		$(".about_this_talk .meta").show();
-		$(".about_this_talk .description").show();
-		$(".about_this_talk .comment_share").show();
-		$(".about_this_talk .ratings").show();	
-		
-		$(".about_this_talk").slideDown();
+		$(this).(".about_this_talk").slideDown();
+		 */
 	},
 	function () {
-		$(".talk").removeClass("active").addClass("inactive");
-
-		$(".about_this_talk .meta").hide();
-		$(".about_this_talk .description").hide();
-		$(".about_this_talk .comment_share").hide();
-		$(".about_this_talk .ratings").hide();	
+		$(this).removeClass("active").addClass("inactive");
+		/*
+		$(this).(".about_this_talk .meta").hide();
+		$(this).(".about_this_talk .description").hide();
+		$(this).(".about_this_talk .comment_share").hide();
+		$(this).(".about_this_talk .ratings").hide();	
+		 */
+	});
+	
+	var $container = $('#video-container');
+	$container.imagesLoaded(function(){
+		$container.masonry({
+  			itemSelector: '.talk',
+  			//columnWidth: 220,
+  			isAnimated: true
+  		});
 	});
 		
 });
